81560ece3c9ec8ce4faca6fffdf32aaac2774767
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-29 23:54:29 +0530

    setup

--- Awards/src/App.jsx
+++ Awards/src/App.jsx
@@ -1,5 +1,11 @@
+import Hero from "./Hero";
+import "./index.css";
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
