358c162fcc8aeead1b7086b1053b4da1f514ccdc
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-31 22:44:03 +0530

    .

--- Awards/src/components/Hero.jsx
+++ Awards/src/components/Hero.jsx
@@ -47,6 +47,24 @@ const Hero = () => {
     { dependencies: [currentIndex], revertOnUpdate: true }
   );
 
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.out",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        scrub: true,
+      },
+    });
+  });
+
   const getVideoSrc = (index) => {
     return `videos/hero-${index}.mp4`;
   };
@@ -54,6 +72,15 @@ const Hero = () => {
 
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-blue-75">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
         className="relative z-10 h-full w-full overflow-hidden rounded-lg bg-blue-75"
@@ -85,7 +112,10 @@ const Hero = () => {
             className="absolute left-0 top-0 size-full object-cover object-center"
           />
         </div>
-        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+        <h1 className=" special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+        <h1 className=" special-font hero-heading absolute bottom-5 right-5 z-40 text-black">
           G<b>a</b>ming
         </h1>
         <div className="absolute left-0 top-0 z-40 size-full">
